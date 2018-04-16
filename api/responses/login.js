54bc09a78c008cfbb17ad9a2a5dced7662691aca
NDT <nguyendtjob@gmail.com>
2018-04-16 11:49:08 +0200

    -bcrypt for the login -Fix the extension check function

--- api/responses/login.js
+++ api/responses/login.js
@@ -9,31 +9,37 @@
  */
 
 module.exports = function login(inputs) {
-  //inputs = inputs || {};
 
   // Get access to `req` and `res`
   var req = this.req;
   var res = this.res;
 
+  const bcrypt = require('bcrypt');
+
   // Look up the user
   User.findOne({
-    email: inputs.email,
-    password: inputs.password
+    email: inputs.email
   }).exec(function (err, user) {
-    if (err) return res.negotiate(err);
+    if (err)
+      return res.negotiate(err);
 
-    // Redirect to the login page if there wasn't any user matching the email and password
+    // Redirect to the login page if there wasn't any user matching the email
     if (!user) {
-
       return res.view('login',{error:"error"});
     }
 
-    // "Remember" the user in the session
-    // Subsequent requests from this user agent will have `req.session.me` set.
-    req.session.me = user.id;
+    //Use bcrypt to compare the password if there was a user found.
+    bcrypt.compare(inputs.password, user.password, function(err, isMatch) {
+      if (err)
+        return res.negotiate(err);
 
-
-    return res.redirect('/Peptide/adminlist');
+      if(isMatch) {
+        req.session.me = user.id;
+        return res.redirect('/Peptide/adminlist');
+      } else {
+        return res.view('login',{error:"error"});
+      }
+    });
   });
 
 };
